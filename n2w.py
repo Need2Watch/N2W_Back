efb995b078d70a5a358528a04ed54407a6a1490c
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2020-05-25 19:07:43 +0200

    Added swagger

--- n2w.py
+++ n2w.py
@@ -3,6 +3,10 @@ import os
 from flask import Flask
 from .config import config
 from tmdbv3api import TMDb
+from flask_swagger_ui import get_swaggerui_blueprint
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from .src.user.application.auth import users
 
@@ -15,14 +19,27 @@ def create_app(environment):
 
 def create_movie_api():
     tmdb = TMDb()
-    tmdb.api_key = os.environ['MOVIE_API_KEY']
+    tmdb.api_key = os.getenv('MOVIE_API_KEY')
     tmdb.language = 'en'
     tmdb.debug = True
     return tmdb
-    
+
+
 environment = config['development']
 
-tmdb = create_movie_api()
 app = create_app(environment)
+tmdb = create_movie_api()
 
+### swagger specific ###
+SWAGGER_URL = ''
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Need2Watch REST API"
+    }
+)
+
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 app.register_blueprint(users)
