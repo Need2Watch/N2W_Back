17323958a88f9545658a8e7d84021428a793c317
José Márquez Doblas <jose.marquez@atlantyasoftware.com>
2020-05-26 12:00:00 +0200

    User id added to movies endpoints

--- src/movie/application/movies.py
+++ src/movie/application/movies.py
@@ -9,45 +9,45 @@ from .service.from_movie_to_dict import FromMovieToDict
 movies = Blueprint("movies", __name__, url_prefix="/movies")
 
 
-@movies.route('/<int:movie_id>', methods=["GET"])
-def get_movie(movie_id: int):
+@movies.route(['/<int:movie_id>', '/<int:movie_id>/<string:user_id>'], methods=["GET"])
+def get_movie(movie_id: int, user_id: str = None):
     movie_repository = MovieRepository()
 
-    movie = movie_repository.getById(movie_id)
+    movie = movie_repository.getById(movie_id, user_id)
     if not movie:
         abort(404)
 
     return jsonify(FromMovieToDict.with_movie(movie))
 
 
-@movies.route('/popular', methods=["GET"])
-def get_popular_movies():
+@movies.route(['/popular', '/popular/<string:user_id>'], methods=["GET"])
+def get_popular_movies(user_id: str = None):
     movie_repository = MovieRepository()
 
-    movies = movie_repository.getPopularMovies()
+    movies = movie_repository.getPopularMovies(user_id)
     if not movies:
         abort(404)
 
     return jsonify(list(map(lambda movie: FromMovieToDict.with_movie(movie), movies)))
 
 
-@movies.route('/similar/<int:movie_id>', methods=["GET"])
-def get_similar_movies(movie_id: int):
+@movies.route(['/similar/<int:movie_id>', '/similar/<int:movie_id>/<string:user_id>'], methods=["GET"])
+def get_similar_movies(movie_id: int, user_id: str = None):
     movie_repository = MovieRepository()
 
-    movies = movie_repository.getSimilarById(movie_id)
+    movies = movie_repository.getSimilarById(movie_id, user_id)
     if not movies:
         abort(404)
 
     return jsonify(list(map(lambda movie: FromMovieToDict.with_movie(movie), movies)))
 
 
-@movies.route('/search', methods=["POST"])
-def search_movie():
+@movies.route(['/search', '/search/<string:user_id>'], methods=["POST"])
+def search_movie(user_id: str = None):
     movie_repository = MovieRepository()
 
     title = request.json.get('title')
-    movies = movie_repository.getByTitle(title)
+    movies = movie_repository.getByTitle(title, user_id)
     if not movies:
         abort(404)
 
