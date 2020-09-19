3d45beea60d26348d561ba7d18995b3b4ea9ff35
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2020-09-20 00:41:07 +0200

    Rename in memory repository and move it to test folder
    Co-authored-by: Francis Molina <francisjmp99@gmail.com>

---
+++ tests/user/infrastructure/user_in_memory_repository.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+
+from ....src.user.domain.user import User
+from ....src.user.domain.user_id import UserId
+from ....src.user.domain.user_repository import UserRepository
+
+
+class UserInMemoryRepository(UserRepository):
+    def __init__(self):
+        self.__users: List[User] = []
+
+    def save(self, user: User):
+        self.__users.append(user)
+
+    def find(self, user_id: UserId) -> Optional[User]:
+        for user in self.__users:
+            if user.user_id.value == user_id.value:
+                return user
+        return None
+
+    def find_by_email(self, user_email: str) -> Optional[User]:
+        for user in self.__users:
+            if user.email == user_email:
+                return user
+        return None
