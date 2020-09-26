52eb10f358c514c953cefb171584de075175d07b
Pablo <pablorodriguezguillen@gmail.com>
2020-09-26 21:32:17 +0200

    Fix vo string parsing
    Co-authored-by: Francis Molina <francisjmp99@gmail.com>

--- src/user/domain/no_existing_user_with_id_error.py
+++ src/user/domain/no_existing_user_with_id_error.py
@@ -3,7 +3,7 @@ from .user_id import UserId
 
 class NoExistingUserWithIdError(Exception):
     def __init__(self, user_id: UserId):
-      self.message = 'User with id: {0} does not exist'.format(user_id)
+      self.message = 'User with id: {0} does not exist'.format(user_id.value)
 
     def __str__(self):
         return self.message
