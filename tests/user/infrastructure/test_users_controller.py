cfd81a420224ad69d11aada9895dad32b61b8052
Pablo <pablorodriguezguillen@gmail.com>
2020-09-27 00:11:52 +0200

    Refactor users controller test

--- tests/user/infrastructure/test_users_controller.py
+++ tests/user/infrastructure/test_users_controller.py
@@ -1,5 +1,4 @@
 import json
-import time
 import uuid
 from typing import Optional
 
@@ -14,57 +13,65 @@ from ..builder.user_builder import UserBuilder
 fake = Faker()
 
 
-user_repository = UserMysqlRepository()
-
-user_id = str(uuid.uuid4())
-username = fake.name()
-password = fake.password()
-first_name = fake.first_name()
-last_name = fake.last_name()
-email = fake.email()
-country = fake.country()
-city = fake.city()
-
-request_params = {
-    "user_id": user_id,
-    "username": username,
-    "password": password,
-    "email": email,
-    "first_name": first_name,
-    "last_name": last_name,
-    "country": country,
-    "city": city
-}
+def teardown_module():
+    UserMysqlRepository().clean()
 
 
-class TestUsersController():
-    def test_users_post(self):
+class TestUsersPostController():
+    def test_should_create_and_save_a_user_with_the_passed_parameters(self):
+        user_email = fake.email()
+        users_post_request_params = get_users_post_request_params_with_email(user_email)
+
         response = app.test_client().post(
             '/users',
-            data=json.dumps(request_params),
+            data=json.dumps(users_post_request_params),
             content_type='application/json'
         )
 
         user_repository = UserMysqlRepository()
+        saved_user = user_repository.find_by_email(user_email)
         assert response.status_code == 200
-        saved_user: Optional[User] = user_repository.find_by_email(email)
         assert saved_user != None
-        assert str(saved_user.user_id.value) == user_id
-        assert saved_user.email == email
+        assert saved_user.username == users_post_request_params["username"]
+        assert saved_user.password.verify(users_post_request_params["password"])
+        assert saved_user.email == users_post_request_params["email"]
+        assert saved_user.first_name == users_post_request_params["first_name"]
+        assert saved_user.last_name == users_post_request_params["last_name"]
+        assert saved_user.city == users_post_request_params["city"]
+        assert saved_user.country == users_post_request_params["country"]
 
-    def test_users_post_returns_409_when_user_already_exists(self):
+    def test_should_return_409_when_creating_a_user_with_an_already_registered_email(self):
+        user_email = fake.email()
+        UserBuilder().with_email(user_email).insert()
+        users_post_request_params = get_users_post_request_params_with_email(user_email)
+
         response = app.test_client().post(
             '/users',
-            data=json.dumps(request_params),
+            data=json.dumps(users_post_request_params),
             content_type='application/json'
         )
 
         assert response.status_code == 409
 
-    def test_users_put(self):
+
+def get_users_post_request_params_with_email(email: str):
+    return {
+        "user_id": str(uuid.uuid4()),
+        "username": fake.user_name(),
+        "password": fake.password(),
+        "email": email,
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "country": fake.country(),
+        "city": fake.city()
+    }
+
+
+class TestUsersPutController():
+    def test_should_update_the_parameters_of_the_user_which_id_is_passed(self):
         user_id = UserId.from_string(str(uuid.uuid4()))
         UserBuilder().with_user_id(user_id).insert()
-        put_users_request_params = {
+        users_put_request_params = {
             "username": fake.name(),
             "first_name": fake.first_name(),
             "last_name": fake.last_name(),
@@ -74,16 +81,15 @@ class TestUsersController():
 
         response = app.test_client().put(
             '/users/{0}'.format(user_id.value),
-            data=json.dumps(put_users_request_params),
+            data=json.dumps(users_put_request_params),
             content_type='application/json'
         )
 
         user_repository = UserMysqlRepository()
         updated_user = user_repository.find(user_id)
-        print(updated_user)
         assert response.status_code == 200
         assert updated_user != None
-        assert updated_user.username == put_users_request_params["username"]
-        assert updated_user.first_name == put_users_request_params["first_name"]
-        assert updated_user.last_name == put_users_request_params["last_name"]
-        assert updated_user.city == put_users_request_params["city"]
+        assert updated_user.username == users_put_request_params["username"]
+        assert updated_user.first_name == users_put_request_params["first_name"]
+        assert updated_user.last_name == users_put_request_params["last_name"]
+        assert updated_user.city == users_put_request_params["city"]
